7a3f722629b5a53c810645d88c511ebf94890cfc
Victor Hugo <vhsds2000@gmail.com>
2024-09-11 15:39:50 -0300

    navbar and footer creation + styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+//CSS
 import './App.css';
+
+//react
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+//pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Mini Blog</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
